42d6fadfb00334328fd4149537b7ca901295a4ba
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-01 18:26:44 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -400,7 +400,7 @@ with main_container:
                         .grid-header {{ fill:#151a24 !important; }}
                         .grid-row {{ fill:#0b0e14 !important; }}
                         .grid-row:nth-child(even) {{ fill:#0f131a !important; }}
-                        .grid-line {{ stroke: #1e2530 !important; stroke-opacity: 0.4 !important; }}
+                        .grid-line {{ stroke: #1e2530 !important; stroke-opacity: 0.1 !important; }}
                         .arrow {{ stroke: #9ca3af !important; stroke-width: 1.6 !important; opacity: 1 !important; fill: none !important; }}
                         .bar-wrapper.imp-urgente .bar {{ fill:#DC2626 !important; }}
                         .bar-wrapper.imp-alta    .bar {{ fill:#F97316 !important; }}
@@ -658,6 +658,7 @@ st.markdown(f"""
     NEXION // LOGISTICS OS // GUADALAJARA, JAL. // © 2026
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
