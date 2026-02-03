df13e7758b8a0e3cc1f3e72b6e9a64e59a5734b9
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-03 00:26:19 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1129,9 +1129,10 @@ st.markdown(f"""
 <div class="footer">
     NEXION // LOGISTICS OS // GUADALAJARA, JAL. // © 2026 <br>
     <span style="opacity:0.5; font-size:8px; letter-spacing:4px;">ENGINEERED BY </span>
-    <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNAN PHY</span>
+    <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNANPHY</span>
 </div>
 """, unsafe_allow_html=True)
+
 
 
 
