4db458effaee434e930dfcd3f37e5acadb2b7f05
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-04 10:45:40 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1487,15 +1487,7 @@ else:
                     }
                     </style>
                 """, unsafe_allow_html=True)
-    
-                # ── ENCABEZADO JYPESA ──
-                st.markdown("""
-                    <div class="main-header">
-                        <h2 style='margin:0; color:#54AFE7;'>SISTEMA NEXION v2.0</h2>
-                        <p style='margin:0; font-size:12px; opacity:0.8;'>JYPESA | Automatización de Procesos de Logística</p>
-                    </div>
-                """, unsafe_allow_html=True)
-                
+                                   
                 # ── CONFIGURACIÓN DE SEGURIDAD ──
                 TOKEN = st.secrets.get("GITHUB_TOKEN", None)
                 REPO_NAME = "RH2026/nexion"
@@ -1593,6 +1585,7 @@ else:
         <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNANPHY</span>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
