411865fe8ef5491bdfd236e2ed5d996a8a22a5d0
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-05 19:28:34 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -80,7 +80,7 @@ if "menu_sub" not in st.session_state:
 vars_css = {
     "bg": "#0E1117",      # Fondo Onix Azulado
     "card": "#1A1F2B",    # Fondos de tarjetas e inputs
-    "text": "##E0E6ED",    # Texto principal
+    "text": "#E0E6ED",    # Texto principal
     "sub": "#D9D9D9",     # Texto secundario
     "border": "#2D333B",  # Bordes y líneas
     "logo": "n1.png"      # Logo
@@ -1880,6 +1880,7 @@ else:
         <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNANPHY</span>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
