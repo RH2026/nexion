5e5144e1c0009aeb522355f333411f09c5acbbf2
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-06 23:45:39 -0600

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -78,7 +78,7 @@ if "menu_sub" not in st.session_state:
     st.session_state.menu_sub = "GENERAL"
 
 vars_css = {
-    "bg": "#0E1420",      # Fondo profundo
+    "bg": "#0C101A",      # Fondo profundo
     "card": "#1F2937",    # Color para las celdas (Azul grisáceo)
     "text": "#F8FAFC",    
     "sub": "#94A3B8",     
@@ -1915,6 +1915,7 @@ else:
         <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNANPHY</span>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
