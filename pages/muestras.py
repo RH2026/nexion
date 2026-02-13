4bc3b9612f644d0e9452e7f09caa38a5fb1b1428
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-13 14:09:13 -0600

    Update muestras.py

--- pages/muestras.py
+++ pages/muestras.py
@@ -417,10 +417,8 @@ else:
         with c1:
             try:
                 st.image(vars_css["logo"], width=160)
-                st.markdown(f"<p style='font-size:8px; letter-spacing:2px; color:{vars_css['sub']}; margin-top:-22px; margin-left:2px;'>SYSTEM SOLUTIONS</p>", unsafe_allow_html=True)
-            except:
-                st.markdown(f"<h3 style='letter-spacing:4px; font-weight:800; margin:0; color:{vars_css['text']};'>NEXION</h3>", unsafe_allow_html=True)
-    
+                st.markdown(f"<p style='text-align:center; font-size:8px; letter-spacing:2px; color:{vars_css['sub']}; margin-top:4px; margin-bottom:0;'>SYSTEM SOLUTIONS</p>", unsafe_allow_html=True)
+                                        
         with c2:
             # INDICADOR GENERAL (CENTRADO ABSOLUTO CON ESPACIADO NEXION)
             if st.session_state.menu_sub != "GENERAL":
@@ -2604,6 +2602,7 @@ else:
         <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNANPHY</span>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
