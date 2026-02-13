38ec538d99e50d85f9524bf46eb51ac48214b571
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-02-13 13:42:58 -0600

    dashboard.py

--- pages/muestras.py
+++ pages/muestras.py
@@ -318,9 +318,9 @@ button[kind="secondary"] {{
 }}
 
 button[kind="secondary"]:hover {{
-    background-color: #00A844 !important;   /* un poco más claro que tu card */
-    color: {vars_css['text']} !important;
-    border-color: #3A4049 !important;
+    background-color: #00A844 !important;
+    color: #FFFFFF !important;
+    border-color: #00A844 !important;
 }}
 
 button[kind="secondary"] {{
@@ -2598,6 +2598,7 @@ else:
         <span style="color:{vars_css['text']}; font-weight:800; letter-spacing:3px;">HERNANPHY</span>
     </div>
     """, unsafe_allow_html=True)
+
 
 
 
