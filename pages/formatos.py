39fe85f9b1ed7ad59d716df3fccdf4351ca8f9e9
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-03-02 17:38:52 -0600

    Update formatos.py

--- pages/formatos.py
+++ pages/formatos.py
@@ -13,8 +13,8 @@ if 'df_calidad_oficial' not in st.session_state:
         [{"FECHA": None, "No de factura": "", "No de parte": "", "Orden fabricacion": "", "Lote": "", "Cantidad": "", "Descripcion": ""}] * 10
     )
 
-# --- CAPTURA DE DATOS (INPUTS AMARILLOS) ---
-st.title("Captura de Formato de Calidad")
+# --- CAPTURA DE DATOS (ESTILO ELITE) ---
+st.title(":material/rebase_edit: Control de Rehabilitación y Reproceso")
 with st.container(border=True):
     c1, c2, c3, c4, c5 = st.columns(5)
     f_solicita = c1.text_input("SOLICITA CALIDAD").upper()
@@ -29,7 +29,7 @@ with st.container(border=True):
     f_guia = c8.text_input("GUÍA").upper()
     f_costo = c9.text_input("COSTO").upper()
 
-st.write("### Detalle de Productos")
+st.write("### :material/grid_view: Detalle de Productos")
 df_edit = st.data_editor(st.session_state.df_calidad_oficial, num_rows="dynamic", use_container_width=True,
                          column_config={"FECHA": st.column_config.DateColumn("FECHA", format="DD/MM/YYYY")})
 
@@ -40,40 +40,36 @@ with st.container(border=True):
     
     f_acciones = st.text_area("ACCIONES A REALIZAR SEGÚN SEA EL CASO").upper()
     
-    ca1, ca2, ca3 = st.columns(3)
+    ca1, ca2, ca3, ca4 = st.columns(4)
     f_mano_obra = ca1.text_input("GASTO: MANO DE OBRA").upper()
     f_hrs = ca2.text_input("HRS").upper()
-    f_otros_gastos = ca3.text_input("OTROS / CANTIDAD").upper()
+    f_otros_gastos = ca3.text_input("OTROS").upper()
+    f_cant_final = ca4.text_input("CANTIDAD").upper()
     
     f_dictamen = st.radio("DICTAMEN FINAL:", ["ACEPTADO", "RECHAZADO"], horizontal=True)
 
-# --- MOTOR DE IMPRESIÓN (REPLICA EXACTA) ---
+# --- MOTOR DE IMPRESIÓN (REPLICA EXACTA SEGÚN IMAGEN) ---
 def generar_html_exacto():
-    # Procesar filas de la tabla de productos
     filas_html = ""
     for _, r in df_edit.iterrows():
         f_val = r['FECHA'].strftime('%d-%b-%y') if r['FECHA'] else ""
         filas_html += f"""
         <tr style="height:20px;">
-            <td>{f_val}</td>
-            <td>{r['No de factura']}</td>
-            <td>{r['No de parte']}</td>
-            <td>{r['Orden fabricacion']}</td>
-            <td>{r['Lote']}</td>
-            <td style="text-align:center;">{r['Cantidad']}</td>
-            <td>{r['Descripcion']}</td>
+            <td style="border:1px solid black;">{f_val}</td>
+            <td style="border:1px solid black;">{r['No de factura']}</td>
+            <td style="border:1px solid black;">{r['No de parte']}</td>
+            <td style="border:1px solid black;">{r['Orden fabricacion']}</td>
+            <td style="border:1px solid black;">{r['Lote']}</td>
+            <td style="border:1px solid black; text-align:center;">{r['Cantidad']}</td>
+            <td style="border:1px solid black;">{r['Descripcion']}</td>
         </tr>"""
     
-    # Relleno para asegurar las filas de la imagen
     for _ in range(max(0, 10 - len(df_edit))):
-        filas_html += "<tr style='height:20px;'><td colspan='7'></td></tr>"
+        filas_html += "<tr style='height:20px;'><td style='border:1px solid black;' colspan='7'></td></tr>"
 
-    # Lógica de checks para Tipo de Orden
     check_ret = "( x )" if "Retrabajo" in f_tipo_orden else "(   )"
     check_rehab = "( x )" if "Rehabilitación" in f_tipo_orden else "(   )"
     check_repro = "( x )" if "Reproceso" in f_tipo_orden else "(   )"
-    
-    # Lógica de dictamen
     dic_acep = "( x )" if f_dictamen == "ACEPTADO" else "(   )"
     dic_rech = "( x )" if f_dictamen == "RECHAZADO" else "(   )"
 
@@ -82,122 +78,91 @@ def generar_html_exacto():
     <head>
         <style>
             @media print {{ @page {{ size: letter landscape; margin: 5mm; }} }}
-            body {{ font-family: 'Arial Narrow', Arial; font-size: 9px; color: black; }}
-            table {{ width: 100%; border-collapse: collapse; table-layout: fixed; }}
+            body {{ font-family: 'Arial Narrow', Arial; font-size: 8.5px; color: black; }}
+            table {{ width: 100%; border-collapse: collapse; border: 1.5px solid black; table-layout: fixed; }}
             td {{ border: 1px solid black; padding: 2px; }}
             .header-gray {{ background-color: #D9D9D9; font-weight: bold; text-align: center; font-size: 8px; }}
             .input-blue {{ color: blue; font-weight: bold; font-style: italic; font-size: 10px; }}
             .title {{ font-size: 13px; font-weight: bold; text-align: center; }}
-            .no-border {{ border: none; }}
         </style>
     </head>
     <body>
         <table>
             <tr>
                 <td colspan="2" style="width:20%; text-align:center;"><b>JYPESA</b></td>
-                <td colspan="9" class="title">Formato de control de rehabilitación, reproceso y retrabajo de producto</td>
+                <td colspan="10" class="title">Formato de control de rehabilitación, reproceso y retrabajo de producto</td>
             </tr>
             <tr class="header-gray">
-                <td>Clave:</td><td>Versión:</td><td colspan="2">Fecha de publicación:</td><td colspan="2">Fecha de próxima revisión:</td><td colspan="5">Sustituye a:</td>
+                <td style="width:10%;">Clave:</td><td style="width:10%;">Versión:</td><td colspan="3">Fecha de publicación:</td><td colspan="3">Fecha de próxima revisión:</td><td colspan="4">Sustituye a:</td>
             </tr>
             <tr style="text-align:center;">
-                <td>F03-PNO-AC-21</td><td>3</td><td colspan="2">14-Ene-25</td><td colspan="2">14-Ene-28</td><td colspan="5">F03-PNO-AC-21 V02</td>
+                <td>F03-PNO-AC-21</td><td>3</td><td colspan="3">14-Ene-25</td><td colspan="3">14-Ene-28</td><td colspan="4">F03-PNO-AC-21 V02</td>
             </tr>
             <tr class="header-gray">
-                <td style="width:9%;">Solicita Calidad:</td>
-                <td style="width:8%;">No de desviación:</td>
-                <td colspan="2">No de retrabajo: / No de reclamo:</td>
-                <td style="width:6%;">No de cliente</td>
-                <td colspan="2">Nombre comercial</td>
-                <td style="width:8%;">Transporte</td>
-                <td style="width:8%;">Guía</td>
-                <td colspan="2">Costo</td>
+                <td>Solicita Calidad:</td><td>No de desviación:</td><td colspan="2">No de retrabajo:</td><td>No de reclamo:</td><td>No de cliente</td><td colspan="2">Nombre comercial</td><td>Transporte</td><td>Guía</td><td colspan="2">Costo</td>
             </tr>
             <tr class="input-blue" style="text-align:center; height:22px;">
-                <td>{f_solicita}</td>
-                <td>{f_desviacion}</td>
-                <td>{f_retrabajo}</td><td>{f_reclamo}</td>
-                <td>{f_cliente}</td>
-                <td colspan="2">{f_nom_com}</td>
-                <td>{f_transp}</td>
-                <td>{f_guia}</td>
-                <td colspan="2">{f_costo}</td>
+                <td>{f_solicita}</td><td>{f_desviacion}</td><td colspan="2">{f_retrabajo}</td><td>{f_reclamo}</td><td>{f_cliente}</td><td colspan="2">{f_nom_com}</td><td>{f_transp}</td><td>{f_guia}</td><td colspan="2">{f_costo}</td>
             </tr>
         </table>
-
         <table style="margin-top:-1px;">
             <tr class="header-gray">
                 <td style="width:10%;">Fecha</td><td style="width:12%;">No de factura</td><td style="width:12%;">No de parte</td><td style="width:12%;">Orden de fabricación</td><td style="width:8%;">Lote</td><td style="width:8%;">Cantidad</td><td>Descripción del producto</td>
             </tr>
             {filas_html}
         </table>
-
         <table style="margin-top:-1px;">
-            <tr class="header-gray">
-                <td colspan="2" style="width:20%;">Orden de:</td>
-                <td style="width:80%;">Comentarios (descripción breve del hallazgo)</td>
-            </tr>
-            <tr>
-                <td style="width:15%;">Retrabajo</td><td style="width:5%; text-align:center;">{check_ret}</td>
-                <td rowspan="3" class="input-blue" valign="top">{f_coment_hallazgo}</td>
-            </tr>
-            <tr><td>Rehabilitación</td><td style="text-align:center;">{check_rehab}</td></tr>
-            <tr><td>Reproceso</td><td style="text-align:center;">{check_repro}</td></tr>
+            <tr class="header-gray"><td style="width:25%;">Orden de:</td><td style="width:75%;">Comentarios (descripción breve del hallazgo)</td></tr>
+            <tr><td>Retrabajo <span style="float:right;">{check_ret}</span></td><td rowspan="3" class="input-blue" valign="top">{f_coment_hallazgo}</td></tr>
+            <tr><td>Rehabilitación <span style="float:right;">{check_rehab}</span></td></tr>
+            <tr><td>Reproceso <span style="float:right;">{check_repro}</span></td></tr>
         </table>
-
         <table style="margin-top:-1px;">
             <tr class="header-gray"><td>Acciones a realizar según sea el caso Retrabajo/Rehabilitación/Reproceso</td></tr>
-            <tr><td class="input-val" style="height:45px;" valign="top">{f_acciones}</td></tr>
+            <tr><td class="input-blue" style="height:40px;" valign="top">{f_acciones}</td></tr>
         </table>
-
+        <table style="margin-top:-1px;">
+            <tr class="header-gray">
+                <td style="width:25%;">Nota de crédito</td><td style="width:5%;"></td><td rowspan="4" valign="top">
+                    <div style="text-align:center; padding:10px;"><b>Analista de incoming</b><br><br><br>___________________________<br>Firma/fecha</div>
+                </td>
+            </tr>
+            <tr><td>Producto</td><td></td></tr>
+            <tr><td>Servicio</td><td></td></tr>
+            <tr style="height:20px;"><td></td><td></td></tr>
+        </table>
         <table style="margin-top:-1px;">
             <tr class="header-gray"><td colspan="3">Seguimiento a la desviación</td></tr>
             <tr style="height:20px;">
-                <td><b>Analista de inventario MP:</b></td>
-                <td><b>Analista de inventario PT:</b></td>
-                <td><b>Programador de producción:</b></td>
+                <td>Analista de inventario MP:</td><td>Analista de inventario PT:</td><td>Programador de producción:</td>
             </tr>
-            <tr class="header-gray">
-                <td colspan="3">Dictamen de retrabajo/rehabilitación/reproceso finalizado: Aceptado {dic_acep} o rechazado {dic_rech}</td>
-            </tr>
+            <tr class="header-gray"><td colspan="3">Dictamen de retrabajo/rehabilitación/reproceso finalizado: Aceptado {dic_acep} o rechazado {dic_rech}</td></tr>
         </table>
-
         <table style="margin-top:-1px;">
             <tr style="height:20px;">
-                <td style="width:33%;">Gasto: mano de obra: <span class="input-blue">{f_mano_obra}</span></td>
-                <td style="width:15%;">hrs: <span class="input-blue">{f_hrs}</span></td>
+                <td>Gasto: mano de obra: <span class="input-blue">{f_mano_obra}</span></td>
+                <td>hrs: <span class="input-blue">{f_hrs}</span></td>
                 <td>Otros: <span class="input-blue">{f_otros_gastos}</span></td>
+                <td>Cantidad: <span class="input-blue">{f_cant_final}</span></td>
             </tr>
+            <tr><td colspan="4" style="height:30px;"><b>Comentarios:</b></td></tr>
         </table>
-
-        <div style="display:flex; justify-content:space-around; margin-top:40px; text-align:center;">
-            <div style="width:30%;">
-                <div style="border-top:1px solid black; width:80%; margin:auto;"></div>
-                <b>Analista de incoming</b><br>Firma/fecha
-            </div>
-            <div style="width:30%;">
-                <div style="border-top:1px solid black; width:80%; margin:auto;"></div>
-                <b>Supervisor de calidad</b><br>Firma/fecha
-            </div>
-            <div style="width:30%;">
-                <div style="border-top:1px solid black; width:80%; margin:auto;"></div>
-                <b>Supervisor de producción</b><br>Firma/fecha
-            </div>
+        <div style="display:flex; justify-content:space-around; margin-top:30px; text-align:center;">
+            <div style="width:40%; border-top:1px solid black;"><b>Supervisor de calidad</b><br>Firma/fecha</div>
+            <div style="width:40%; border-top:1px solid black;"><b>Supervisor de producción</b><br>Firma/fecha</div>
         </div>
-
-        <div style="margin-top:15px; display:flex; justify-content:space-between; font-size:8px;">
-            <span>Formato: F02-PNO-SGC-02 Versión 01</span>
-            <span>Página 1 de 1</span>
+        <div style="margin-top:10px; display:flex; justify-content:space-between; font-size:8px;">
+            <span>Formato: F02-PNO-SGC-02 Versión 01</span><span>Página 1 de 1</span>
         </div>
     </body>
     </html>
     """
     return html_template
 
-# BOTÓN DE IMPRESIÓN
-if st.button("IMPRIMIR FORMATO REPLICA", type="primary", use_container_width=True):
+if st.button(":material/print: IMPRIMIR FORMATO REPLICA", type="primary", use_container_width=True):
     formato = generar_html_exacto()
     components.html(f"<html><body>{formato}<script>window.onload = function() {{ window.print(); }}</script></body></html>", height=0)
+
 
 
 
