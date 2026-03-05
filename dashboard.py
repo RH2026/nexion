5e7826343bb7d6a5cec6b552b3a03da95cf27f72
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-03-05 16:42:42 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -857,8 +857,11 @@ else:
                             <div class="kpi-ruta-card" style="background: rgba(255,255,255,0.05); border-top: 4px solid {accent_color}; position: relative; padding: 20px; border-radius: 4px;">
                                 <div style="display: flex; justify-content: space-between; align-items: center; margin-bottom: 20px;">
                                     <span style="color: {accent_color}; font-weight: 800; font-size: 14px; letter-spacing: 1px;">DETALLES DE OPERACIÓN</span>
-                                    <span style="font-size:18px; font-weight:600;">FACTURA: </span>
-                                    <span style="color:{accent_color}; font-weight:800; font-size:22px;">{d['NÚMERO DE PEDIDO']}</span>
+                                    
+                                    <div style="display: flex; align-items: baseline; gap: 8px;">
+                                        <span style="font-size:18px; font-weight:600; color: white;">FACTURA:</span>
+                                        <span style="color:{accent_color}; font-weight:800; font-size:22px;">{d['NÚMERO DE PEDIDO']}</span>
+                                    </div>
                                 </div>
                                 <div class="kpi-route-flow" style="margin-bottom: 25px;">
                                     <div class="city" style="color: white; font-weight:bold;">GDL</div>
@@ -4088,6 +4091,7 @@ else:
         </div>
     """, unsafe_allow_html=True)
     
+
 
 
 
