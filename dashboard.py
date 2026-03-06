93f38c7be45e3815c51f326dea531ef9d67fa788
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-03-05 20:16:10 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -312,7 +312,7 @@ div[data-baseweb="select"] > div:first-child {{
 
 /* 2. Ajuste del texto interno y el cursor */
 div[data-baseweb="select"] div {{
-    font-size: 12px !important;
+    font-size: 10px !important;
     color: {vars_css['text']} !important;
     line-height: 1 !important;
     text-transform: uppercase !important;
@@ -4185,6 +4185,7 @@ else:
         </div>
     """, unsafe_allow_html=True)
     
+
 
 
 
