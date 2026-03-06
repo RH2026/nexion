bf0cb3a048a3ffd3d691bd6ef10c8e9b844e2fa6
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-03-05 20:19:31 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -301,8 +301,8 @@ div[data-baseweb="tag"] svg {{
 
 /* 1. Altura y alineación de la caja principal */
 div[data-baseweb="select"] > div:first-child {{
-    height: 38px !important; 
-    min-height: 38px !important;
+    height: 35px !important; 
+    min-height: 35px !important;
     background-color: {vars_css['card']} !important;
     border: 1px solid {vars_css['border']} !important;
     border-radius: 4px !important;
@@ -4185,6 +4185,7 @@ else:
         </div>
     """, unsafe_allow_html=True)
     
+
 
 
 
