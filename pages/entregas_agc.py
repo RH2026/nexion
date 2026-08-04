7aa5c69f931a860c7427773f695110e331bd34bf
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-08-04 13:01:50 -0600

    Update entregas_agc.py

--- pages/entregas_agc.py
+++ pages/entregas_agc.py
@@ -482,6 +482,8 @@ with header_zone:
                             st.session_state.busqueda_activa = False
                             if s == "ASIGNAR FLETERA":
                                 st.switch_page("pages/asignacionfletera.py")
+                            elif s == "ETIQUETAS":
+                                st.switch_page("pages/etiquetas.py")
                             else:
                                 st.rerun()
         
