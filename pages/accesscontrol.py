f02dc9851f5742d6978532332e9358139df20133
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-08-12 17:47:20 -0600

    Update accesscontrol.py

--- pages/accesscontrol.py
+++ pages/accesscontrol.py
@@ -15,6 +15,9 @@ from pypdf import PdfReader, PdfWriter
 import qrcode
 import streamlit.components.v1 as components
 import streamlit as st
+from auth import exigir_autenticacion
+
+exigir_autenticacion("accesscontrol")
 
 # 1. CONFIGURACIÓN DE PÁGINA
 st.set_page_config(
