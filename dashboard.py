001674bd48146e53edfa5c17ef00f8ee4a837735
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-08-13 17:53:02 -0600

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -16,6 +16,7 @@ import qrcode
 import streamlit.components.v1 as components
 import streamlit as st
 from auth import exigir_autenticacion
+import log
 
 # ==========================================
 # 2. CONTROL DE ACCESO DIRECTO EN RAÍZ
