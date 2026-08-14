d4a08dda4e2e6bf7e4010149b7e75f6b0f9977e1
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-08-13 18:49:36 -0600

    Update accesscontrol.py

--- pages/accesscontrol.py
+++ pages/accesscontrol.py
@@ -723,7 +723,7 @@ with header_zone:
                             st.session_state.menu_sub = s
                             st.session_state.busqueda_activa = False
                             if s == "ASIGNAR FLETERA":
-                                st.switch_page("pages/asignacionfletera.py")
+                                st.switch_page("pages/facturacion_af.py")
                             elif s == "ETIQUETAS":
                                 st.switch_page("pages/etiquetas.py")
                             elif s == "ESCANEAR QR":
