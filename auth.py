83db7c72a9efad32ded9363ac0968935ddd67150
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-08-17 22:59:01 -0600

    Update auth.py

--- auth.py
+++ auth.py
@@ -17,6 +17,7 @@ PAGINAS = {
     "qrup": "pages/qrup.py",
     "facturacion": "pages/facturacion_af.py",
     "main": "pages/main.py",
+    "cargardt": "pages/cargardt.py",
 }
 
 # ============================================================
