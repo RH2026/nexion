7a2b076b7793dd493bcbd105b8a24b46a27e070f
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-10 12:50:17 -0600

    Update layout.py

--- components/layout.py
+++ components/layout.py
@@ -492,6 +492,8 @@ def render_layout(modulo_actual: str, submodulo_actual: str = "GENERAL"):
                                     st.switch_page("pages/preguia_paqmex.py")
                                 elif s == "RECOLECCION 3G":
                                     st.switch_page("pages/recoleccion_3g.py")
+                                elif s == "COTIZACIONES":
+                                    st.switch_page("pages/cotizaciones.py")
                                 else:
                                     st.toast(f"Módulo {s} en desarrollo...", icon="🚧")
                                     st.rerun()
