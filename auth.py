59cea7b1cb66a0acf3005a7d6428149e65fd7dfb
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-09 22:36:54 -0600

    Add analisis_mensual to navigation dictionary

--- auth.py
+++ auth.py
@@ -24,6 +24,7 @@ PAGINAS = {
     "check_agc": "pages/check_agc.py",
     "preguia_paqmex": "pages/preguia_paqmex.py",
     "salida_pt": "pages/salida_pt.py",
+    "analisis_mensual" : "pages/analisis_mensual.py",
 }
 
 # ============================================================
