63db226fabbee6e31fe5775178b45cabde5a4d05
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-09 22:22:02 -0600

    Fix key format in navigation dictionary

--- auth.py
+++ auth.py
@@ -22,7 +22,8 @@ PAGINAS = {
     "recoleccion_3g": "pages/recoleccion_3g.py",
     "tracking": "pages/tracking.py",
     "check_agc": "pages/check_agc.py",
-    "preguia_paqmex.py": "pages/preguia_paqmex.py",
+    "preguia_paqmex": "pages/preguia_paqmex.py",
+    "salida_pt": "pages/salida_pt.py",
 }
 
 # ============================================================
