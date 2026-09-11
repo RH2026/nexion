9b47727afbee15911d27649d4abf2a1fb470b80e
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-11 17:57:09 -0600

    Update layout.py

--- components/layout.py
+++ components/layout.py
@@ -87,9 +87,9 @@ def render_layout(modulo_actual: str, submodulo_actual: str = "GENERAL"):
     }}
     
    div.stButton > button, div.stDownloadButton > button {{
-        background-color: #4E6772 !important;
+        background-color: #628290 !important;
         color: #ffffff !important;
-        border: 1px solid #4E6772 !important;
+        border: 1px solid #628290 !important;
         border-radius: 4px !important;
         font-weight: 700 !important;
         text-transform: uppercase;
@@ -100,9 +100,9 @@ def render_layout(modulo_actual: str, submodulo_actual: str = "GENERAL"):
     }}
     
     div.stButton > button:hover, div.stDownloadButton > button:hover {{
-        background-color: #628290 !important;
+        background-color: #4E6772 !important;
         color: #ffffff !important;
-        border-color: #628290 !important;  
+        border-color: #4E6772 !important;  
     }}
         
     /* --- SEPARACIÓN EQUILIBRADA EN EL POPOVER --- */
