b003f075dce534a921d089481fdea5bd753ab135
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-11 13:05:22 -0600

    Change button background and border colors

--- components/layout.py
+++ components/layout.py
@@ -78,9 +78,9 @@ def render_layout(modulo_actual: str, submodulo_actual: str = "GENERAL"):
     }}
 
     div.stButton > button, div.stDownloadButton > button {{
-        background-color: #71919F !important;
+        background-color: #628290 !important;
         color: #ffffff !important;
-        border: 1px solid #71919F !important;
+        border: 1px solid #628290 !important;
         border-radius: 4px !important;
         font-weight: 700 !important;
         text-transform: uppercase;
