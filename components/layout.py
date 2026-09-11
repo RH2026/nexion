06f2ef853a51896508559ad6b32583555d96d733
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-11 12:52:41 -0600

    Fix CSS background color and border color

--- components/layout.py
+++ components/layout.py
@@ -78,9 +78,9 @@ def render_layout(modulo_actual: str, submodulo_actual: str = "GENERAL"):
     }}
 
     div.stButton > button, div.stDownloadButton > button {{
-        background-color: ##0C769E !important;
+        background-color: #0C769E !important;
         color: #ffffff !important;
-        border: 1px solid #00A3A3 !important;
+        border: 1px solid #0C769E !important;
         border-radius: 4px !important;
         font-weight: 700 !important;
         text-transform: uppercase;
