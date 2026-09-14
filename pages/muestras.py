5ad1551a28a577ca50f1b91c55029219353cd3d6
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-14 11:41:34 -0600

    Update muestras.py

--- pages/muestras.py
+++ pages/muestras.py
@@ -32,7 +32,7 @@ st.set_page_config(
 # ============================================================
 # 2. LLAMADA AL LAYOUT MAESTRO Y PERMISOS
 # ============================================================
-render_layout(modulo_actual="REPORTES", submodulo_actual="MUESTRAS")
+render_layout(modulo_actual="REPORTES", submodulo_actual="ENVIO DE MUESTRAS")
 
 
 # ==========================================
