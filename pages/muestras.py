e58593a7b1ed085ab16fe58c299ea9cfabb5049d
Rigobertto Hernandez <rigobertto.hernandez@gmail.com>
2026-09-14 12:09:08 -0600

    Update muestras.py

--- pages/muestras.py
+++ pages/muestras.py
@@ -1541,9 +1541,9 @@ def main():
                             )
     else:        
         vars_css = {
-            "bg_card" = "#263238"
-            "bg_border" = "rgba(255,255,255,0.05)"
-            "fg_text" = "#FFFFFF"
+            "card": = "#263238"
+            "border": = "rgba(255,255,255,0.05)"
+            "text": = "#FFFFFF"
                 
         }
         
